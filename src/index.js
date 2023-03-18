2257ea2c48cb2d63ab0e36d13a3a69fb3d5e6470
Eduard Malyshenko <ed54rus@mail.ru>
2023-03-19 02:46:57 +0700

    fix 404

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -9,7 +9,7 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<Provider store={store}>
 			<App />
 		</Provider>
